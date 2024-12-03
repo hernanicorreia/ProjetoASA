d84cc9bec8f14d28dafbc627494557b4bcf321f2
Calhau <pedro.d.andrade@tecnico.ulisboa.pt>
2024-12-03 14:42:13 +0000

    tesseract

--- Projeto1.cpp
+++ Projeto1.cpp
@@ -3,18 +3,24 @@
 
 
 int main(){
-    int lad_matx, n_inpt, n_resul;
+    int lad_matx, n_inpt, n_resul, num;
+    std::string line;
+    std::getline(std::cin, line);
+    std::sscanf(line, %d %d, &lad_matx, &n_inpt);
 
-    std::vector<std::vector<int>> matrix(lad_matx, std::vector<int>(lad_matx, 0));
+    std::vector<std::vector<int>> matrixeq(lad_matx, std::vector<int>(lad_matx, 0));
 
     // Print the matrix
     for (int i = 0; i < lad_matx; ++i) {
+        std::getline(std::cin, line);
+        std::stringstream ss(line);
         for (int j = 0; j < lad_matx; ++j) {
-            std::cout << matrix[i][j] << " ";
+            ss >> matrixeq[i][j];
         }
-        std::cout << std::endl;
     }
 
+    std::vector<std::vector<std::vector<std::vector<int>>>> matrixsol(n_inpt, std::vector<std::vector<int>>(n_inpt, std::vector<int>(lad_matx, std::vector<int>(4, 0))));
+    
     
 }
 
