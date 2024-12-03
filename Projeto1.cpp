86589d70a55ff9628d80dba2a75a5376425d9e01
Calhau <pedro.d.andrade@tecnico.ulisboa.pt>
2024-12-03 17:22:55 +0000

    tesseract só se for nigga por nigga por nigga por nigga

--- Projeto1.cpp
+++ Projeto1.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include <sstream>
 
+
+
+
 int main() {
     int lad_matx, n_inpt, n_resul, num;
     std::string line;
@@ -9,6 +12,22 @@ int main() {
     std::sscanf(line.c_str(), "%d %d", &lad_matx, &n_inpt);
 
     std::vector<std::vector<int>> matrixeq(lad_matx, std::vector<int>(lad_matx, 0));
+
+    int add_sol(int sol, int k, int sol_e, int sol_d, int col, int lin){
+        for(i = 0; i < lad_matx; i++){
+            if(matrixsol[lin][col][i][0]==0){
+                matrixsol[lin][col][i][0] = sol;
+                matrixsol[lin][col][i][1] = k;
+                matrixsol[lin][col][i][2] = sol_e;
+                matrixsol[lin][col][i][3] = sol_d;
+                return 1
+            }
+            if(matrixsol[lin][col][i][0] == sol)
+                return 0;
+        }
+        return -1
+    }
+
 
     // Input the matrix
     for (int i = 0; i < lad_matx; ++i) {
@@ -39,11 +58,32 @@ int main() {
         int sol_esq = eq[i]-1;
         int sol_dir = eq[j]-1;
         int sol = matrixeq[sol_esq][sol_dir];
-        matrixsol[i][j][sol-1][0] = sol;
-        matrixsol[i][j][sol-1][2] = sol_esq +1;
-        matrixsol[i][j][sol-1][3] = sol_dir +1;
+        matrixsol[i][j][0][0] = sol;
+        matrixsol[i][j][0][2] = sol_esq +1;
+        matrixsol[i][j][0][3] = sol_dir +1;
         }
+    int counter = 2;
+    while(matrixsol[0][n_inpt-1][0][0] == 0){
+        for (int i = 0; i < n_inpt; i++){
+            int j = i + counter;
+            if(j > n_inpt){
+                break;
+            }
+            for(int k = counter; k < 0; k--){
+                std::vector<std::vector<int>> sol_e = matrixsol[i][k-1];
+                std::vector<std::vector<int>> sol_d = matrixsol[k][j];
+                for(const std::vector<int>& vec_e : sol_e ){
+                    for(const std::vector<int>& vec_d : sol_d){
+                        int sol = eq[vec_e[0]-1][vec_d[0]-1];
+                        add_sol(sol, k, vec_e[0], vec_d[0], i, j);
 
+                    }
+                }
+            }
+        }
+    }
+    #resolve
+    
 
     std::getline(std::cin, line);
     std::sscanf(line.c_str(), "%d", &n_resul);
